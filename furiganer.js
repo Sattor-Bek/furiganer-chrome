55db4230a3c8fea0b5a3686ecc43fdef58af124b
Sattor-Bek <satoru.nh@gmail.com>
2022-01-01 20:07:35 +0900

    fetch api

--- furiganer.js
+++ furiganer.js
@@ -1,3 +1,5 @@
+importScripts("./contentScript.js");
+
 function showAlert(text) {
 	let str = "";
 	if(text){
@@ -8,6 +10,39 @@ function showAlert(text) {
 	alert(str);
 } // this function will be replaced
 
+
+async function postData(input){
+	const body = {
+		id: userId,
+		jsonrpc: "2.0",
+		method: method,
+		params: {
+			q: input,
+			grade: 1
+		}
+	}
+	const request = {
+		method: "POST",
+		mode: "no-cors",
+		body: JSON.stringify(body)
+	}
+
+	const response = await fetch(baseUrl, request);
+	return response.json()
+}
+
+function convert(input) {
+	let response = {}
+	postData(input).then(res => {
+		console.log("start coverting...")
+		response = res;
+		console.log("finished converting")
+	})
+	const result = JSON.stringify(response);
+	return result.result;
+}
+
+
 chrome.action.onClicked.addListener((tab) => {
 	chrome.scripting.executeScript({
 		target: { tabId: tab.id },
@@ -17,7 +52,7 @@ chrome.action.onClicked.addListener((tab) => {
 
 chrome.runtime.onInstalled.addListener(() => {
 	chrome.contextMenus.create({
-		"id":"jnhjpmkcbhgdmlnpbppbgmagpmojamea",
+		"id": extensionId,
 		"title":"Furiganer",
 		"contexts":["selection"],
 	});
@@ -25,12 +60,12 @@ chrome.runtime.onInstalled.addListener(() => {
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
 	const text = info.selectionText;
-	//put furigana
+	const textData = convert(text)
 	//inject the text with furigana to clipboard
 		chrome.scripting.executeScript({
 			target: { tabId: tab.id },
 			function: showAlert,
-			args:[text]
+			args:[textData]
 		});		
 
 });
